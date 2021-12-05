7528545e69058a30fb872ff65b911d854cfe37dd
bigfatball <95281151+bigfatball@users.noreply.github.com>
2021-12-05 08:42:08 +0800

    Update submit_test.php

--- submit_test.php
+++ submit_test.php
@@ -1,58 +1,57 @@
+<!DOCTYPE html>
 <html>
-
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 </head>
-
-
 <body>
-
-<div>
-<input id = "NAME" type="text" >
-<button type="button" id = "SUBMIT">submit</button>
-<button type="button" id = "TEST">test</button>
-<p>This is a paragraph.</p>
-
-</div>
-
+<form>
+  <table>
+    <tr><td>username:</td><td>
+    <input type="text" name="NAME" id="NAME"/></td></tr>  
+    <tr><td>
+     <input type="button" value="送出" onClick="MessageGo();"/>   
+     </td></tr>
+  </table>
+ </form>
 </body>
 
 <script>
-    $(document).ready(function(){
-        $("#SUBMIT").on("click", function(){
-            $ajax({
-                type:"POST",
-                dataType:"Json",
-                contentType:"application/Json;charset=utf-8",
-                url:"database_test.php"
-                data: JSON.stringift({
-                    NAME:$('#NAME').val()
-                }),
-            });
-           
-        })
-    });
+  function MessageGo(){
+//取得 "username" 欄位值
+//var goods = $('#goods').val();
+    document.getElementById('NAME').value  = "yoyo"  
+    var goods = document.getElementById('NAME').value       
+ //取得 "message" 欄位值     
+                                     
 
-    
+  $.ajax({
+      //告訴程式表單要傳送到哪裡                                         
+      url:"database_test.php",                                                              
+      //需要傳送的資料
+      data:"&NAME="+NAME,
+       //使用POST方法     
+      type : "POST",                                                                    
+      //接收回傳資料的格式，在這個例子中，只要是接收true就可以了
+      dataType:'json', 
+       //傳送失敗則跳出失敗訊息      
+      error:function(){                                                                 
+      //資料傳送失敗後就會執行這個function內的程式，可以在這裡寫入要執行的程式  
+      alert("失敗");
+      },
+      //傳送成功則跳出成功訊息
+      success:function(){                                                           
+      //資料傳送成功後就會執行這個function內的程式，可以在這裡寫入要執行的程式  
+      alert("成功");
+      }
+  }); 
+
+};
+                        
+
 
 
 </script>
 
-
-<script>
-    $(document).ready(function(){
-        $("#TEST").click(function(){
-            $("p").slideToggle();
-
-    });
-});
-    
-
-
-</script>
 
 </html>
 
